23818c1f8f4c74cd7ce825c479c7d6aa11932c08
Ajdin Imsirovic <ajdika@live.com>
2024-01-28 04:22:07 +0100

    Green: Extend Appointment component to display customer details

--- src/Appointment.js
+++ src/Appointment.js
@@ -1,15 +1,45 @@
 import React from "react";
+
+// Function to format the appointment time
 export const appointmentTimeOfDay = (startsAt) => {
   const [h, m] = new Date(startsAt).toTimeString().split(":");
   return `${h}:${m}`;
 };
 
-// Basic implementation of the Appointment component
+// Implementation of the Appointment component
 const Appointment = ({ customer }) => {
-
-  // Existing exports and components
-
-  return <div>{customer.firstName}</div>;
+  return (
+    <div>
+      <table>
+        <tbody>
+          <tr>
+            <th>First Name</th>
+            <td>{customer.firstName}</td>
+          </tr>
+          <tr>
+            <th>Last Name</th>
+            <td>{customer.lastName}</td>
+          </tr>
+          <tr>
+            <th>Phone Number</th>
+            <td>{customer.phoneNumber}</td>
+          </tr>
+          <tr>
+            <th>Stylist</th>
+            <td>{customer.stylist}</td>
+          </tr>
+          <tr>
+            <th>Service</th>
+            <td>{customer.service}</td>
+          </tr>
+          <tr>
+            <th>Notes</th>
+            <td>{customer.notes}</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  );
 };
 
 export default Appointment;
