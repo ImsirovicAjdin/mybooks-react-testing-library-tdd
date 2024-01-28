9e9e68e8c024bbd19127b2dd3a35d188359d385c
Ajdin Imsirovic <ajdika@live.com>
2024-01-28 04:46:11 +0100

    Refactor: sampleData no longer pollutes Array.prototype;simplify fake data

--- src/sampleData.js
+++ src/sampleData.js
@@ -11,36 +11,16 @@
 //   { startsAt: at(16), customer: { firstName: "Jules" } },
 //   { startsAt: at(17), customer: { firstName: "Stevie" } },
 // ];
-
 import { faker } from "@faker-js/faker";
 
-Array.prototype.unique = function () {
-  return this.filter(function (value, index, self) {
-    return self.indexOf(value) === index;
-  });
-};
-
-Array.prototype.pickRandom = function () {
-  return this[
-    Math.floor(Math.random() * this.length)
-  ];
-};
+const unique = (array) => array.filter((v, i, a) => a.indexOf(v) === i);
+const pickRandom = (array) => array[Math.floor(Math.random() * array.length)];
 
 const today = new Date();
 const at = (hours) => today.setHours(hours, 0);
 
-const stylists = [0, 1, 2, 3, 4, 5, 6]
-  .map(() => faker.person.firstName())
-  .unique();
-
-const services = [
-  "Cut",
-  "Blow-dry",
-  "Cut & color",
-  "Beard trim",
-  "Cut & beard trim",
-  "Extensions",
-];
+const stylists = unique(Array.from({ length: 7 }, () => faker.person.firstName()));
+const services = ["Cut", "Blow-dry", "Cut & color", "Beard trim", "Cut & beard trim", "Extensions"];
 
 const generateFakeCustomer = () => ({
   firstName: faker.person.firstName(),
@@ -50,19 +30,12 @@ const generateFakeCustomer = () => ({
 
 const generateFakeAppointment = () => ({
   customer: generateFakeCustomer(),
-  stylist: stylists.pickRandom(),
-  service: services.pickRandom(),
+  stylist: pickRandom(stylists),
+  service: pickRandom(services),
   notes: faker.lorem.paragraph(),
 });
 
-export const sampleAppointments = [
-  { startsAt: at(9), ...generateFakeAppointment() },
-  { startsAt: at(10), ...generateFakeAppointment() },
-  { startsAt: at(11), ...generateFakeAppointment() },
-  { startsAt: at(12), ...generateFakeAppointment() },
-  { startsAt: at(13), ...generateFakeAppointment() },
-  { startsAt: at(14), ...generateFakeAppointment() },
-  { startsAt: at(15), ...generateFakeAppointment() },
-  { startsAt: at(16), ...generateFakeAppointment() },
-  { startsAt: at(17), ...generateFakeAppointment() },
-];
+export const sampleAppointments = Array.from({ length: 9 }, (_, i) => ({
+  startsAt: at(9 + i),
+  ...generateFakeAppointment(),
+}));
