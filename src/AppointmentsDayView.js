2e1a454984b98aca73e7ac817f6a77d1f27e1651
Ajdin Imsirovic <ajdika@live.com>
2024-01-28 02:28:01 +0100

    Green: Add AppointmentsDayView.js component with data-testid

---
+++ src/AppointmentsDayView.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const AppointmentsDayView = ({ appointments }) => {
+  return (
+    <div data-testid="appointmentsDayView">
+      {/* Future content goes here */}
+    </div>
+  );
+};
+
+export default AppointmentsDayView;
--- src/test/AppointmentsDayView.test.js
+++ src/test/AppointmentsDayView.test.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
-import AppointmentsDayView from '../src/AppointmentsDayView';
+import AppointmentsDayView from '../AppointmentsDayView';
 
 describe("AppointmentsDayView", () => {
     it("renders a div with the right id", () => {
