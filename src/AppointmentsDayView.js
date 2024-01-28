394ebc0a1d338f52f3a502aae253f03b82ae5abf
Ajdin Imsirovic <ajdika@live.com>
2024-01-28 04:01:49 +0100

    Green: AppointmentsDayView renders another appointment when button is clicked

--- src/AppointmentsDayView.js
+++ src/AppointmentsDayView.js
@@ -1,13 +1,15 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Appointment, { appointmentTimeOfDay } from './Appointment';
 
 const AppointmentsDayView = ({ appointments }) => {
+  const [selectedAppointment, setSelectedAppointment] = useState(0);
+
   return (
     <div data-testid="appointmentsDayView">
         <ol>
-        {appointments.map(appointment => (
+        {appointments.map((appointment, i) => (
             <li key={appointment.startsAt}>
-                <button type="button">
+                <button type="button" onClick={() => setSelectedAppointment(i)}>
                     {appointmentTimeOfDay(appointment.startsAt)}
                 </button>
             </li>
@@ -16,7 +18,7 @@ const AppointmentsDayView = ({ appointments }) => {
         {appointments.length === 0 ? (
             <p>There are no appointments scheduled for today.</p>
         ) : (
-            <Appointment {...appointments[0]} />
+            <Appointment {...appointments[selectedAppointment]} />
         )}
     </div>
   );
